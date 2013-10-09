eccbe44b5cc165930830f4d8794ea47a796679b5
Aki Karkkainen <etimiti@gmail.com>
2013-10-09 14:11:51 +0200

    Adapted Karma configuration in karma.conf.js, to follow the latest updated syntax.

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 /*
- * Karma configuration.
+ * Karma configuration. Generated on Wed Oct 09 2013 12:04:13 GMT+0200 (CEST)
  * 
- * @author Aki Karkkainen
+ * @author Aki Karkkainen (edits)
  * @url https://github.com/akikoo/grunt-frontend-workflow
  * Twitter: http://twitter.com/akikoo
  * 
@@ -9,91 +9,73 @@
  * See http://karma-runner.github.io/0.8/plus/RequireJS.html
  */
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+module.exports = function (config) {
 
+    config.set({
 
-// list of files / patterns to load in the browser
-files = [
+        // Base path, that will be used to resolve files and exclude.
+        basePath: '',
 
-    // Adapters
-    JASMINE,
-    JASMINE_ADAPTER,
-    REQUIRE,
-    REQUIRE_ADAPTER,
+        // Frameworks to use.
+        frameworks: ['jasmine', 'requirejs'],
 
-    // Which files do you want to test?
-    // 
-    // Choose all the files we want to load with Require.js.
-    {pattern: 'www/js/lib/**/*.js', included: false},       // All external libraries.
-    {pattern: 'www/js/app/**/*.js', included: false},       // Our source code.
-    {pattern: 'test/spec/*Spec.js', included: false},       // All the tests.
+        // list of files / patterns to load in the browser.
+        files: [
+            // Which files do you want to test?
+            // 
+            // Choose all the files we want to load with Require.js.
+            {pattern: 'www/js/lib/**/*.js', included: false},   // All external libraries.
+            {pattern: 'www/js/app/**/*.js', included: false},   // Our source code.
+            {pattern: 'test/spec/*Spec.js', included: false},   // All the tests.
 
-    // Which files do you want to include with <script> tag?
-    // 
-    // Choose all files which are not loaded by Require.js. 
-    // Usually you'll only need to include your test-main.js file, 
-    // which has the same role for your tests as main.js has for 
-    // your app when using Require.js.
-    'test/test-main.js'
-];
+            // Which files do you want to include with <script> tag?
+            // 
+            // Choose all files which are not loaded by Require.js. 
+            // Usually you'll only need to include your test-main.js file, 
+            // which has the same role for your tests as main.js has for 
+            // your app when using Require.js.
+            'test/test-main.js'
+        ],
 
+        // List of files to exclude.
+        // We don't want to actually start the application in our tests.
+        exclude: [
+            'www/js/app/mainpage.js',
+            'www/js/app/subpage.js'
+        ],
 
+        // Test results reporter to use.
+        // Possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+        reporters: ['progress'],
 
-// list of files to exclude
-// We don't want to actually start the application in our tests.
-exclude = [
-    'www/js/app/mainpage.js',
-    'www/js/app/subpage.js'
-];
+        // Web server port.
+        port: 9876,
 
+        // Enable / disable colors in the output (reporters and logs).
+        colors: true,
 
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['dots'];
+        // Level of logging.
+        // Possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
+        // Enable / disable watching file and executing tests whenever any file changes.
+        autoWatch: false,
 
-// web server port
-port = 9876;
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari (only Mac)
+        // - PhantomJS
+        // - IE (only Windows)
+        browsers: ['Firefox'],
 
+        // If browser does not capture in given timeout [ms], kill it.
+        captureTimeout: 60000,
 
-// cli runner port
-runnerPort = 9100;
-
-
-// enable / disable colors in the output (reporters and logs)
-colors = true;
-
-
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
-
-
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
-
-
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = [
-    // 'Chrome', 
-    // 'PhantomJS' 
-    // 'Safari', 
-    'Firefox'
-];
-
-
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 60000;
-
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+        // Continuous Integration mode.
+        // if true, it capture browsers, run tests and exit.
+        singleRun: false
+    });
+};
