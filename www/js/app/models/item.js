cd61b5fe3b777e2012bf69247a5ac4ad426419ba
Aki Karkkainen <etimiti@gmail.com>
2013-10-13 17:54:08 +0200

    Added idAttribute, to map from MongoDB unique key to id.

--- www/js/app/models/item.js
+++ www/js/app/models/item.js
@@ -4,38 +4,36 @@
 
 define(['backbone'],
 
-    function(Backbone) {
+    function (Backbone) {
 
         'use strict';
 
         var Model = Backbone.Model.extend({
-
             // Default attribute values.
             defaults: {
                 name: '',
                 description: ''
             },
 
+            // Set a Model's idAttribute to transparently map from MongoDB unique key to id.
+            idAttribute: '_id',
+
             // Initialize and listen for changes to the model.
             initialize: function () {
-
                 // console.log('This model has been initialized.');
 
                 this.on('change', function () {
                     // console.log('Values for this model have changed.');
                 });
-
             },
 
             // Gets called automatically by Backbone when the set and/or save methods are called.
             validate: function (attrs) {
                 // console.log('Before attempting to save.');
             }
-
         });
 
         return Model;
-
     }
 
 );
